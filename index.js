8799a487ab16d23130f6e5558db5e200e98fdd95
Alex Revell <alex.revell@youdo.co.nz>
2017-07-28 16:19:37 +1200

    update to add core for methods to act as generators, thanks Wouter

--- index.js
+++ index.js
@@ -1,43 +1,163 @@
-const take = require('./take')
-const takeUntil = require('./takeUntil')
-const log = require('./log')
-const first = require('./first')
-const last = require('./last')
+// const take = require('./take')
+// const takeUntil = require('./takeUntil')
+// const log = require('./log')
+// const first = require('./first')
+// const last = require('./last')
+require('./itrabble')
 
 const array = ['a','b','c','d','e','f']
-const map = new Map([['a', 'a'],['b', 'b'], ['c', 'c'],['d','d'],['e','e'],['f','f']])
-const string = 'test string'
+// const map = new Map([['a', 'a'],['b', 'b'], ['c', 'c'],['d','d'],['e','e'],['f','f']])
+// const string = 'test string'
 
-Object.assign(Array.prototype, { first, last, log, take, takeUntil })
-Object.assign(Map.prototype, { first, last, log, take, takeUntil })
-Object.assign(String.prototype, { first, last, log, take, takeUntil })
+// function withConstructor(constructor){
+//   return o => {
+//     const proto = Object.assign({},
+//       Object.getPrototypeOf(o),
+//       { constructor }
+//     )
+//     return Object.assign(Object.create(proto), o)
+//   }
+// }
+//
+// function pipe(...fns) {
+//   return x => fns.reduce((y, f) => f(y), x)
+// }
 
-// const aryTake = array.take(4)
-// const mapTake = map.take(2)
-const stringTake = string.take(2)
+// export default function Itrabble(context){
+//   if(!this || this.constructor !== Itrabble) return new Itrabble(context)
+//   this.context = context
+//   this[Symbol.iterator] = function*(){
+//     for(let val of context) yield val
+//   }
+// }
+//
+// Object.defineProperty(Object.prototype, 'itrabble', {
+//   get: function(){
+//     return Itrabble(this)
+//   }
+// })
 
-// const aryFirst = array.first(2)
-// const mapFirst = map.first(2)
+//
+// function takeIterable(n, iterable){
+//   return Object.assign({
+//     [Symbol.iterator]: () => {
+//       const iterator = iterable[Symbol.iterator]()
+//
+//       return {
+//         next: () => {
+//           let { done, value } = iterator.next()
+//           done = done || n-- <= 0
+//           return {
+//             done,
+//             value: done ? undefined : value
+//           }
+//         }
+//       }
+//     }
+//   }, Itrabble)
+// }
+//
+// function firstIterable(iterable){
+//   const iterator = iterable[Symbol.iterator]()
+//   return iterator.next().value
+// }
+//
+// function untilIterable(fn, iterable) {
+//   return Object.assign({
+//     [Symbol.iterator]: () => {
+//       const iterator = iterable[Symbol.iterator]()
+//
+//       return {
+//         next: () => {
+//           let { done, value } = iterator.next()
+//           done = done || fn(value)
+//           return {
+//             done,
+//             value: done ? undefined : value
+//           }
+//         }
+//       }
+//     }
+//   }, Itrabble)
+// }
+//
+// function filterWithIterable(fn, iterable){
+//   return Object.assign({
+//     [Symbol.iterator]: () => {
+//       const iterator = iterable[Symbol.iterator]()
+//
+//       return {
+//         next: () => {
+//           do {
+//             var { done, value } = iterator.next()
+//           } while (!done && !fn(value))
+//           return { done, value }
+//         }
+//       }
+//     }
+//   }, Itrabble)
+// }
+//
+// function keysIterable(iterable){
+//   return Object.assign({
+//     [Symbol.iterator]: () => {
+//       const iterator = iterable[Symbol.iterator]()
+//
+//       return {
+//         next: () => {
+//           const { done, value: [key] } = iterator.next()
+//           return { done, value: key }
+//         }
+//       }
+//     }
+//   }, Itrabble)
+// }
+//
+// // const makeItrabble = iteratee => pipe(
+// //   firstable,
+// //   withConstructor(makeItrabble)
+// // )(iteratee)
+//
+// const Itrabble = {
+//   take(n = 1) {
+//     return takeIterable(n, this)
+//   },
+//   takeUntil(fn) {
+//     return untilIterable(fn, this)
+//   },
+//   filterWith(fn) {
+//     return filterWithIterable(fn, this)
+//   },
+//   first() {
+//     return firstIterable(this)
+//   },
+//   keys() {
+//     return keysIterable(this)
+//   },
+//   toArray() {
+//     return Array.from(this)
+//   },
+//   toMap() {
+//     return new Map(this)
+//   },
+//   toObject() {
+//     const proto = Object.assign({}, Object.getPrototypeOf(this))
+//     return Object.assign(Object.create(proto), this)
+//   },
+//   toStr() {
+//     return String(...this)
+//   },
+//   log() {
+//     console.log(this)
+//     return this
+//   },
+// }
+//
+// Object.assign(Object.prototype, Itrabble)
+// Object.assign(Array.prototype, Itrabble)
+// Object.assign(Map.prototype, Itrabble)
+// Object.assign(String.prototype, Itrabble)
 
-// const aryTakeUntil = array.takeUntil(x => x === 'e')
-// const mapTakeUntil = map.takeUntil(x => x.includes('e'))
-const stringTakeUntil = string.takeUntil(x => x === 'i')
-
-// const aryLast = array.last()
-// const mapLast = map.last()
-const stringLast = string.last()
-
-// Array.from(aryTake).log()
-// Array.from(mapTake).log()
-Array.from(stringTake).log()
-
-// Array.from(aryFirst).log()
-// Array.from(mapFirst).log()
-
-// Array.from(aryLast).log()
-// Array.from(mapLast).log()
-// Array.from(stringLast).log()
-
-// Array.from(aryTakeUntil).log()
-// Array.from(mapTakeUntil).log()
-Array.from(stringTakeUntil).log()
+// array.take(1)
+let ary = array.itrabble.takeWhile(x => x !== 'e')
+console.log(ary)
---
+++ itrabble.js
@@ -0,0 +1,161 @@
+function Itrabble(context){
+  if(!this || this.constructor !== Itrabble) return new Itrabble(context)
+  this.context = context
+  this[Symbol.iterator] = function*(){
+    for(let val of context) yield val
+  }
+}
+Object.defineProperty(Object.prototype, 'itrabble', {
+  get: function(){
+    return Itrabble(this)
+  }
+})
+
+Itrabble.prototype.toArray = function(){
+  return Array.from(this)
+}
+
+Itrabble.prototype.buildIterator = function(iteratorFunc){
+  return new Itrabble({[Symbol.iterator]: iteratorFunc.bind(this) })
+}
+
+Itrabble.prototype.last = function(){
+  return this.buildIterator(function*(){
+    let elm;
+    for(elm of this.context){}
+    yield elm
+  })
+}
+
+Itrabble.prototype.takeUntil = function(callback){
+  return this.buildIterator(function*(){
+    let taking = true
+    for(let elm of this.context){
+      taking = taking && !callback(elm)
+      if(!taking) break
+      yield elm
+    }
+  })
+}
+
+Itrabble.prototype.takeWhile = function(callback){
+  return this.buildIterator(function*(){
+    let taking = true
+    for(let elm of this.context){
+      taking = taking && callback(elm)
+      if(!taking) break
+      yield elm
+    }
+  })
+}
+
+Itrabble.prototype.take = function(count){
+  return this.buildIterator(function*(){
+    for(let elm of this.context){
+      if(count-- == 0) break
+      yield elm
+    }
+  })
+}
+
+Itrabble.prototype.skipUntil = function(callback){
+  return this.buildIterator(function*(){
+    let skipping = true
+    for(let elm of this.context){
+      skipping = skipping && !callback(elm)
+      if(!skipping)
+        yield elm
+    }
+  })
+}
+
+Itrabble.prototype.skipWhile = function(callback){
+  return this.buildIterator(function*(){
+    let skipping = true
+    for(let elm of this.context){
+      skipping = skipping && callback(elm)
+      if(!skipping)
+        yield elm
+    }
+  })
+}
+
+Itrabble.prototype.skip = function(count){
+  return this.buildIterator(function*(){
+    for(let elm of this.context){
+      if(count-- > 0) continue;
+      yield elm
+    }
+  })
+}
+
+Itrabble.prototype.forEach = function(callback) {
+  return this.buildIterator(function*(){
+    for(let elm of this.context){
+      callback(elm)
+      yield elm
+    }
+  })
+}
+
+Itrabble.prototype.every = function(n, offset=0) {
+  return this.buildIterator(function*(){
+    let index = 0
+    for(let elm of this.context){
+      if(index % n == offset)
+        yield elm
+      index += 1
+    }
+  })
+}
+
+Itrabble.prototype.map = function(callback) {
+  return this.buildIterator(function*(){
+    for(let elm of this.context){
+      yield callback(elm)
+    }
+  })
+}
+
+Itrabble.prototype.filter = function(callback) {
+  return this.buildIterator(function*(){
+    for(let elm of this.context){
+      if(callback(elm)){
+        yield elm
+      }
+    }
+  })
+}
+
+Itrabble.prototype.reject = function(callback) {
+  return this.buildIterator(function*(){
+    for(let elm of this.context){
+      if(!callback(elm)){
+        yield elm
+      }
+    }
+  })
+}
+
+Itrabble.prototype.reduce = function(memo, callback){
+  return this.buildIterator(function*(){
+    for(let elm of this.context){
+      memo = callback(memo, elm)
+    }
+    yield memo
+  })
+}
+
+Itrabble.prototype.zip = function(...its){
+  return this.buildIterator(function*(){
+    const iterators = [this[Symbol.iterator](),...its.map(it => it[Symbol.iterator]())]
+    while(true){
+      let next = iterators.map(it => it.next())
+      if(next.every(elm => elm.done))
+        break
+      yield(next.map(elm => elm.value))
+    }
+  })
+}
+
+module.exports = Itrabble
