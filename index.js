7e6db05e5c6f2eab7541b91a2afedcb4ebd54870
Alex Revell <alex.revell@youdo.co.nz>
2017-07-28 16:21:24 +1200

    remove lots of outdated comments

--- index.js
+++ index.js
@@ -9,95 +9,7 @@ const array = ['a','b','c','d','e','f']
 // const map = new Map([['a', 'a'],['b', 'b'], ['c', 'c'],['d','d'],['e','e'],['f','f']])
 // const string = 'test string'
 
-// function withConstructor(constructor){
-//   return o => {
-//     const proto = Object.assign({},
-//       Object.getPrototypeOf(o),
-//       { constructor }
-//     )
-//     return Object.assign(Object.create(proto), o)
-//   }
-// }
-//
-// function pipe(...fns) {
-//   return x => fns.reduce((y, f) => f(y), x)
-// }
 
-// export default function Itrabble(context){
-//   if(!this || this.constructor !== Itrabble) return new Itrabble(context)
-//   this.context = context
-//   this[Symbol.iterator] = function*(){
-//     for(let val of context) yield val
-//   }
-// }
-//
-// Object.defineProperty(Object.prototype, 'itrabble', {
-//   get: function(){
-//     return Itrabble(this)
-//   }
-// })
-
-//
-// function takeIterable(n, iterable){
-//   return Object.assign({
-//     [Symbol.iterator]: () => {
-//       const iterator = iterable[Symbol.iterator]()
-//
-//       return {
-//         next: () => {
-//           let { done, value } = iterator.next()
-//           done = done || n-- <= 0
-//           return {
-//             done,
-//             value: done ? undefined : value
-//           }
-//         }
-//       }
-//     }
-//   }, Itrabble)
-// }
-//
-// function firstIterable(iterable){
-//   const iterator = iterable[Symbol.iterator]()
-//   return iterator.next().value
-// }
-//
-// function untilIterable(fn, iterable) {
-//   return Object.assign({
-//     [Symbol.iterator]: () => {
-//       const iterator = iterable[Symbol.iterator]()
-//
-//       return {
-//         next: () => {
-//           let { done, value } = iterator.next()
-//           done = done || fn(value)
-//           return {
-//             done,
-//             value: done ? undefined : value
-//           }
-//         }
-//       }
-//     }
-//   }, Itrabble)
-// }
-//
-// function filterWithIterable(fn, iterable){
-//   return Object.assign({
-//     [Symbol.iterator]: () => {
-//       const iterator = iterable[Symbol.iterator]()
-//
-//       return {
-//         next: () => {
-//           do {
-//             var { done, value } = iterator.next()
-//           } while (!done && !fn(value))
-//           return { done, value }
-//         }
-//       }
-//     }
-//   }, Itrabble)
-// }
-//
 // function keysIterable(iterable){
 //   return Object.assign({
 //     [Symbol.iterator]: () => {
@@ -112,7 +24,7 @@ const array = ['a','b','c','d','e','f']
 //     }
 //   }, Itrabble)
 // }
-//
+
 // // const makeItrabble = iteratee => pipe(
 // //   firstable,
 // //   withConstructor(makeItrabble)
@@ -152,12 +64,6 @@ const array = ['a','b','c','d','e','f']
 //     return this
 //   },
 // }
-//
-// Object.assign(Object.prototype, Itrabble)
-// Object.assign(Array.prototype, Itrabble)
-// Object.assign(Map.prototype, Itrabble)
-// Object.assign(String.prototype, Itrabble)
 
-// array.take(1)
 let ary = array.itrabble.takeWhile(x => x !== 'e')
 console.log(ary)
