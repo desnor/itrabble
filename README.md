0624e673d95fbdce866132f4d6b8734584b610b2
alexrevell <revellalex@gmail.com>
2017-07-29 13:17:49 +1200

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# itrabble
-Library to extend JS iterables
+## itrabble
+# Library to extend JS iterables
 
-examples:
+# examples:
 
 ```array.itrabble.skipUntil(x => x === 'd') // d e f```
+
 ```map.itrabble.takeUntil(x => x.includes('e')) // [ 'a', 'a' ] [ 'b', 'b' ] [ 'c', 'c' ] [ 'd', 'd' ]```
+
 ```string.itrabble.takeUntil(x => x === 'i') // t e s t   s t r```
+
 ```console.log(...array.itrabble.take(3).zip(array.itrabble.first(), map.itrabble.skip(1).take(3))) // [ 'a', 'a', [ 'b', 'b' ] ] [ 'b', undefined, [ 'c', 'c' ] ] [ 'c', undefined, [ 'd', 'd' ] ]```
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const map = new Map([['a', 'a'],['b', 'b'], ['c', 'c'],['d','d'],['e','e'],['f',
 const string = 'test string'
 
 console.log(...array.itrabble.skipUntil(x => x === 'd')) // d e f
+
 console.log(...map.itrabble.takeUntil(x => x.includes('e'))) // [ 'a', 'a' ] [ 'b', 'b' ] [ 'c', 'c' ] [ 'd', 'd' ]
+
 console.log(...string.itrabble.takeUntil(x => x === 'i')) // t e s t   s t r
+
 console.log(...array.itrabble.take(3).zip(array.itrabble.first(), map.itrabble.skip(1).take(3))) // [ 'a', 'a', [ 'b', 'b' ] ] [ 'b', undefined, [ 'c', 'c' ] ] [ 'c', undefined, [ 'd', 'd' ] ]
