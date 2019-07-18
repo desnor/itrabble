9dde97b04b66c048c48d97a902f9af45dc964083
alexrevell <revellalex@gmail.com>
2019-07-18 21:31:03 +1000

    write small load test script for comparison with standard Array transformations

---
+++ test-script.js
@@ -0,0 +1,51 @@
+const it = require('./')
+const map = require('./lib/pipeable/map')
+const filter = require('./lib/pipeable/filter')
+const first = require('./lib/pipeable/first')
+const toArray = require('./lib/pipeable/first')
+
+const timeArray = (length) => {
+    console.time(`array ${length}`);
+    const res = Array.from({ length }, (_, i) => i)
+        .map(x =>  x ** x )
+        .filter(x => x % 3 === 0)[0];
+    console.timeEnd(`array ${length}`);
+    return res
+}
+
+function* genList(n){
+    let i = 0
+    while(i <= n) {
+        yield i
+        i++
+    }
+}
+
+const timeItrabble = (length) => {
+    console.time(`itrabble ${length}`);
+    const res = it(genList(length))
+        .map(x => x ** x )
+        .filter(x => x % 3 === 0)
+        .first()
+        .toArray();
+    console.timeEnd(`itrabble ${length}`);
+    return res
+}
+
+const timePipeItrabble = (length) => {
+    console.time(`pipeable ${length}`)
+    const res = it(genList(length)).pipe(
+        map(x => x ** x),
+        filter(x => x % 3 === 0 ),
+        first(),
+        toArray()
+    )
+    console.timeEnd(`pipeable ${length}`)
+    return res
+}
+
+const n = 10000000
+
+timeArray(n)
+timeItrabble(n)
+timePipeItrabble(n)
