e38bc6e59bebf583ef7b2976b2cc76a6acca974e
alexrevell <revellalex@gmail.com>
2019-10-28 19:21:28 +1100

    remove unused index.js file, update rollup config to bundle into commonjs & esm targets based on easiest adoption formula, update .gitignore

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,44 @@ import compiler from '@ampproject/rollup-plugin-closure-compiler'
 import multiEntry from 'rollup-plugin-multi-entry'
 import pkg from './package.json'
 
-export default {
-  input: ['lib/index.js', 'lib/pipeable/*.js'],
+export default [{
+    // umd browser build
+    input: ['lib/index.js', 'lib/pipeable/*.js'],
+    output: {
+      file: pkg.browser,
+      format: 'umd',
+      name: 'Itrabble',
+    },
+    plugins: [
+      multiEntry(),
+      compiler(),
+    ]
+  },
+  // pipeable build
+  {
+    input: 'lib/pipeable/*.js',
+    output: [{
+      file: 'commonjs/pipeable/index.js',
+      format: 'cjs',
+    },{
+      file: 'esm/pipeable/index.js',
+      format: 'es'
+    }],
+    plugins: [
+      multiEntry(),
+    ]
+  },
+  // main cjs and esm build
+  {
+  input: ['lib/index.js'],
   output: [{
     file: pkg.main,
     format: 'cjs',
   }, {
     file: pkg.module,
     format: 'es',
-  }, {
-    file: pkg.browser,
-    format: 'umd',
-    name: 'Itrabble',
   }],
   plugins: [
-    multiEntry(),
-    compiler(),
+    // compiler(),
   ]
-};
+}]
