a84819edc59ab85e1fdad73c469d2bc7aee2097f
Alex Revell <alex.revell@mudbath.com.au>
2020-01-16 13:35:02 +1100

    adding some methods to types defintion, add .vscode workspace settings to gitignore, bump up nvmrc version to 13.6

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,18 @@
-export as namespace Itrabble;
-export = Itrabble;
+type PredicateFunction<T> = (value: T, index: number) => boolean;
+type VariadicCallback<T> = (...values: T[]) => void;
 
-declare module itrabble {
-  export type Itrabble<T> = IterableIterator<T> & {
+type BinaryCallback<T> = (value: T, index: number) => void;
+
+export declare module itrabble {
+  export type Itrabble<T extends any> = {
+    [Symbol.iterator](): IterableIterator<T>;
     pipe(...functions: Function[]): IterableIterator<T> | Iterable<T>;
+    toArray(): T[];
+    toSet(): Set<T>;
+    eachChunk(n: number, callback: VariadicCallback<T>): Itrabble<T> | never;
+    filter(callback: PredicateFunction<T>): Itrabble<T>;
+    first(): Itrabble<T>;
+    forEach(callback: BinaryCallback<T>): Itrabble<T>;
   };
 
   export function of<T>(...args: T[]): Itrabble<T>;
