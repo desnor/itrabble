efdefbe8c1600e1bacc34a29d4175e39ec511bb9
Alex Revell <alex.revell@mudbath.com.au>
2020-01-23 14:16:03 +1100

    WIP updating pipe typings, having trouble.

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,16 @@
 type PredicateFunction<T> = (value: T, index: number) => boolean;
+type UnaryFunction<T, R> = (value: T) => R;
 type VariadicCallback<T> = (...values: T[]) => void;
-
 type BinaryCallback<T> = (value: T, index: number) => void;
 
-export declare module itrabble {
+export namespace itrabble {
   export type Itrabble<T extends any> = {
     [Symbol.iterator](): IterableIterator<T>;
-    pipe(...functions: Function[]): IterableIterator<T> | Iterable<T>;
+    pipe(
+      ...functions: ((item: T) => any)[]
+    ): IterableIterator<any> | Iterable<any>;
     toArray(): T[];
+    toMap<K extends keyof T>(): Map<K, T> | Map<T, K>;
     toSet(): Set<T>;
     eachChunk(n: number, callback: VariadicCallback<T>): Itrabble<T> | never;
     filter(callback: PredicateFunction<T>): Itrabble<T>;
@@ -18,3 +21,10 @@ export declare module itrabble {
   export function of<T>(...args: T[]): Itrabble<T>;
   export function from<T>(context: Iterable<T>): Itrabble<T>;
 }
+
+// }
+
+// const a = Itrabble.from([1, 2, 3, 4]).pipe(
+//   x => x * 2,
+//   x => x * 3
+// );
