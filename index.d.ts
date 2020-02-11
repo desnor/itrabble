42156eb7ec860a48d7acbd7954194a60f45b6e9d
Alex Revell <alex.revell@mudbath.com.au>
2020-02-11 14:27:51 +1100

    WIP still, getting closer to pipe definition

--- index.d.ts
+++ index.d.ts
@@ -2,29 +2,38 @@ type PredicateFunction<T> = (value: T, index: number) => boolean;
 type UnaryFunction<T, R> = (value: T) => R;
 type VariadicCallback<T> = (...values: T[]) => void;
 type BinaryCallback<T> = (value: T, index: number) => void;
+type IterableType<T> =
+  | Array<T>
+  | Set<T>
+  | Map<K extends keyof T ? infer K : never, T>;
 
-export namespace itrabble {
-  export type Itrabble<T extends any> = {
+declare module "itrabble" {
+  class Itrabble<T extends any> {
     [Symbol.iterator](): IterableIterator<T>;
-    pipe(
-      ...functions: ((item: T) => any)[]
-    ): IterableIterator<any> | Iterable<any>;
+    // pipe(
+    //   ...functions: ((item: T) => R extends any)[]
+    // ): (context: IterableIterator<T>) => Itrabble<R> | IterableType<R>;
+    pipe<A>(
+      op1: UnaryFunction<T, A>
+    ): (context: IterableIterator<T>) => Itrabble<A> | IterableType<A>;
+    pipe<A, B>(
+      op1: UnaryFunction<T, A>,
+      op2: UnaryFunction<A, B>
+    ): (context: IterableIterator<T>) => Itrabble<B> | IterableType<B>;
+    pipe<A, B, C>(
+      op1: UnaryFunction<T, A>,
+      op2: UnaryFunction<A, B>,
+      op3: UnaryFunction<B, C>
+    ): (context: IterableIterator<T>) => Itrabble<C> | IterableType<C>;
     toArray(): T[];
-    toMap<K extends keyof T>(): Map<K, T> | Map<T, K>;
+    toMap<T, K extends keyof T>(): Map<K, T> | Map<T, K>;
     toSet(): Set<T>;
     eachChunk(n: number, callback: VariadicCallback<T>): Itrabble<T> | never;
     filter(callback: PredicateFunction<T>): Itrabble<T>;
     first(): Itrabble<T>;
     forEach(callback: BinaryCallback<T>): Itrabble<T>;
-  };
+  }
 
   export function of<T>(...args: T[]): Itrabble<T>;
   export function from<T>(context: Iterable<T>): Itrabble<T>;
 }
-
-// }
-
-// const a = Itrabble.from([1, 2, 3, 4]).pipe(
-//   x => x * 2,
-//   x => x * 3
-// );
