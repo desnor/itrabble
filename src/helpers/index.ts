8a6da78f13d56ac5e363058d9db538cea3f0f346
Alex Revell <alex.revell@mudbath.com.au>
2023-02-22 14:15:03 +1100

    refactor(types): updates to try and get export paths typed correctly (esp. pipeable exports)

--- src/helpers/index.ts
+++
@@ -1,7 +0,0 @@
-import Itrabble from '../itrabble';
-
-export function isIterable<T>(
-  value: any
-): value is Itrabble<T> | Iterable<T> | IterableIterator<T> {
-  return Boolean(value[Symbol.iterator]);
-}
